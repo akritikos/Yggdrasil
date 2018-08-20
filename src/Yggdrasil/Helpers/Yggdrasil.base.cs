cc201eaff1a8efc5c9e2ef7ce2a795ebee3bb20a
Αλέξανδρος Κρητικός <akritikos@outlook.com>
2018-08-20 16:33:22 +0300

    Adds base class to hold static members for generic Yggdrasil implementation

---
+++ src/Yggdrasil/Helpers/Yggdrasil.base.cs
@@ -0,0 +1,12 @@
+namespace Kritikos.MachineLearning.Forest.Helpers
+{
+	using System;
+
+	/// <summary>
+	/// Base type containing fields used by generic Yggdrasil implementation
+	/// </summary>
+	public class Yggdrasil
+	{
+		private protected static readonly Random Rnd = new Random();
+	}
+}
